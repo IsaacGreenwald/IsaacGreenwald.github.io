c8845391ba3af9c394d05b868a8527922da6cc3b
Isaac Greenwald <i_greenwald@coloradocollege.edu>
2025-12-04 09:16:41 -0700

    fix bio

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
     <div className="home">
       
       <p className="home-text">
-        I am a senior at Colorado College studying computer science and statistics. For my senior thesis, I created an analytical dashboard for 
+        I recently graduated from Colorado College with a major in computer science and a minor in statistics. For my senior thesis, I created an analytical dashboard for 
         the Colorado College AI Taskforce showing how students are using AI in CC classes. Additionally, I completed a ten week REU at Montana State University
         investigating the capabilities of the packet sniffing system Snort. I am currently looking for work in artificial intelligence, cybersecurity, 
         machine learning and data science. In my free time, I love rock climbing, photography, spending time in the darkroom, and skiing. 
